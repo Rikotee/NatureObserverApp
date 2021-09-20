2987d31688c465abcaf6856c3889f63396b6cbc8
Rikotee <teerek81@gmail.com>
2021-09-20 18:09:48 +0300

    token test

--- app/src/main/java/com/example/natureobserverapp/MainActivity.kt
+++ app/src/main/java/com/example/natureobserverapp/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.d("TEST", "Testing!")
         Log.d("TEST", "Testing again")
+        Log.d("TEST", "Testing token")
     }
 }
