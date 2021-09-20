e0a0132afcb9d4967ea5b7c49630daea7bf75e75
Rikotee <teerek81@gmail.com>
2021-09-20 18:43:58 +0300

    Bottom navigation tab

--- app/src/main/java/com/example/natureobserverapp/MainActivity.kt
+++ app/src/main/java/com/example/natureobserverapp/MainActivity.kt
@@ -2,14 +2,33 @@ package com.example.natureobserverapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("TEST", "Testing!")
-        Log.d("TEST", "Testing again")
-        Log.d("TEST", "Testing token")
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+        val thirdFragment = ThirdFragment()
+
+        setCurrentFragment(firstFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> setCurrentFragment(firstFragment)
+                R.id.person -> setCurrentFragment(secondFragment)
+                R.id.settings -> setCurrentFragment(thirdFragment)
+            }
+            true
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
 }
