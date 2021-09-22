262950d2f9192bc430fef68066bb2c8111f282f8
Rikotee <teerek81@gmail.com>
2021-09-22 14:17:48 +0300

    Bottom navigation tab

---
+++ app/src/main/java/com/example/natureobserverapp/FirstFragment.kt
@@ -0,0 +1,6 @@
+package com.example.natureobserverapp
+
+import androidx.fragment.app.Fragment
+
+class FirstFragment:Fragment(R.layout.fragment_first) {
+}
---
+++ app/src/main/java/com/example/natureobserverapp/ThirdFragment.kt
@@ -0,0 +1,6 @@
+package com.example.natureobserverapp
+
+import androidx.fragment.app.Fragment
+
+class ThirdFragment:Fragment(R.layout.fragment_third) {
+}
