d82661f60b8b1a376b402095b7695b74cd9e3a6c
TuomasB73 <tuomasbergholm@gmail.com>
2021-09-24 14:23:17 +0300

    Database start

---
+++ app/src/main/java/com/example/natureobserverapp/database.kt
@@ -0,0 +1,100 @@
+package com.example.natureobserverapp
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Entity
+data class NatureObservation(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val title: String,
+    val category: String,
+    val description: String,
+    val picturePath: String,
+    val dateAndTime: String,
+    val locationLat: Double,
+    val locationLon: Double,
+    val lightValue: Double
+)
+
+@Entity(foreignKeys = [ForeignKey(
+    entity = NatureObservation::class,
+    onDelete = ForeignKey.CASCADE,
+    parentColumns = ["id"],
+    childColumns = ["observationId"])])
+data class WeatherInfo(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val observationId: Long,
+    val description: String,
+    val icon: String,
+    val temp: Double,
+    val pressure: Long,
+    val humidity: Long,
+    val windSpeed: Long,
+    val windDeg: Long,
+    val country: String,
+    val placeName: String
+)
+
+class NatureObservationWithWeatherInfo {
+    @Embedded
+    var natureObservation: NatureObservation? = null
+    @Relation(parentColumn = "id", entityColumn = "observationId")
+    var weatherInfo: List<WeatherInfo>? = null
+}
+
+@Dao
+interface NatureObservationDao {
+    @Query("SELECT * FROM natureobservation")
+    fun getAll(): LiveData<List<NatureObservation>>
+
+    @Query("SELECT * FROM natureobservation WHERE natureobservation.id = :natureObservationId")
+    fun getNatureObservationWithWeatherInfo(natureObservationId: Long): NatureObservationWithWeatherInfo
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(natureobservation: NatureObservation): Long
+
+    @Update
+    fun update(natureobservation: NatureObservation)
+
+    @Delete
+    fun delete(natureobservation: NatureObservation)
+}
+
+@Dao
+interface WeatherInfoDao {
+    @Query("SELECT * FROM weatherinfo")
+    fun getAll(): LiveData<List<WeatherInfo>>
+
+    @Query("SELECT * FROM weatherinfo WHERE weatherinfo.id = :weatherInfoId")
+    fun getWeatherInfosOfNatureObservation(weatherInfoId: Long): LiveData<List<WeatherInfo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(weatherinfo: WeatherInfo): Long
+
+    @Update
+    fun update(weatherinfo: WeatherInfo)
+
+    @Delete
+    fun delete(weatherinfo: WeatherInfo)
+}
+
+@Database(entities = [(NatureObservation::class), (WeatherInfo::class)], version = 1)
+abstract class NatureObservationDB: RoomDatabase() {
+    abstract fun natureObservationDao(): NatureObservationDao
+    abstract fun weatherInfoDao(): WeatherInfoDao
+
+    companion object {
+        private var sInstance: NatureObservationDB? = null
+        @Synchronized
+        fun get(context: Context): NatureObservationDB {
+            if (sInstance == null) {
+                sInstance = Room.databaseBuilder(context.applicationContext, NatureObservationDB::class.java,
+                    "nature_observations.db").build()
+            }
+            return sInstance!!
+        }
+    }
+}
