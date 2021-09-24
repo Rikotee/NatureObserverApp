4a54e71251bd1dbc126ba4f832a4f777370b44e9
Rikotee <teerek81@gmail.com>
2021-09-24 11:39:49 +0300

    Category

---
+++ app/src/main/java/com/example/natureobserverapp/Categories.kt
@@ -0,0 +1,7 @@
+package com.example.natureobserverapp
+
+object Categories {
+
+    val categories = arrayOf("Animal", "Bird", "Fish", "Insect", "Tree", "Plant", "Flower", "Rock", "Nest", "Cocoon", "Spider")
+
+}
