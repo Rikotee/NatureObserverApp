2c9c2528244857083792c7ebd517b234256dd654
TuomasB73 <tuomasbergholm@gmail.com>
2021-10-04 12:56:38 +0300

    Small improvements and error handling

--- app/src/main/java/com/example/natureobserverapp/MainActivity.kt
+++ app/src/main/java/com/example/natureobserverapp/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.natureobserverapp
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.FrameLayout
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.example.natureobserverapp.fragment.HomeFragment
@@ -17,6 +18,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity(), HomeFragment.HomeFragmentListener {
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var flFragment: FrameLayout
+    private lateinit var picPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,45 +43,27 @@ class MainActivity : AppCompatActivity(), HomeFragment.HomeFragmentListener {
         }
     }
 
-    override fun onNewObservationButtonClick(picturePath: String) {
-        val bundle = bundleOf("picPath" to picturePath)
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            replace<NewObservationFragment>(R.id.flFragment, args = bundle)
-            addToBackStack(null)
-        }
+    override fun onNewObservationButtonClick(picturePath: String, photoURI: Uri) {
+        picPath = picturePath
+        takePicture.launch(photoURI)
     }
 
     private fun setCurrentFragment(fragment: Fragment) {
-        if (fragment is HomeFragment) {
-            if (supportFragmentManager.backStackEntryCount == 0) {
-                supportFragmentManager.commit {
-                    setReorderingAllowed(true)
-                    replace(R.id.flFragment, fragment)
-                }
-            } else {
-                supportFragmentManager.popBackStack(
-                    supportFragmentManager.getBackStackEntryAt(0).id,
-                    FragmentManager.POP_BACK_STACK_INCLUSIVE
-                )
-            }
-        } else {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                replace(R.id.flFragment, fragment)
-                addToBackStack("tabNavigationFragment")
-            }
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.flFragment, fragment)
         }
     }
 
-    override fun onBackPressed() {
-        if (supportFragmentManager.backStackEntryCount > 0 &&
-            supportFragmentManager.getBackStackEntryAt(supportFragmentManager.backStackEntryCount - 1).name == "tabNavigationFragment"
-        ) {
-            supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
-            bottomNavigationView.selectedItemId = R.id.home
-        } else {
-            super.onBackPressed()
+    private val takePicture =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { result ->
+            if (result) {
+                val bundle = bundleOf("picPath" to picPath)
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    replace<NewObservationFragment>(R.id.flFragment, args = bundle)
+                    addToBackStack(null)
+                }
+            }
         }
-    }
 }
--- app/src/main/java/com/example/natureobserverapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/natureobserverapp/fragment/HomeFragment.kt
@@ -9,6 +9,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,7 +17,6 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -33,12 +33,11 @@ import com.example.natureobserverapp.model.WeatherViewModel
 import java.io.File
 
 class HomeFragment : Fragment(), LocationListener {
-    private lateinit var mCurrentPhotoPath: String
     internal var activityCallBack: HomeFragmentListener? = null
     private lateinit var viewModel: WeatherViewModel
 
     interface HomeFragmentListener {
-        fun onNewObservationButtonClick(picturePath: String)
+        fun onNewObservationButtonClick(picturePath: String, photoURI: Uri)
     }
 
     override fun onAttach(context: Context) {
@@ -71,17 +70,7 @@ class HomeFragment : Fragment(), LocationListener {
         (requireActivity() as AppCompatActivity).supportActionBar?.title =
             getString(R.string.home_title_text)
 
-        if ((Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(
-                requireContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED)
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                0
-            )
-        }
+        checkLocationPermission()
 
         val lm = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 500f, this)
@@ -100,48 +89,58 @@ class HomeFragment : Fragment(), LocationListener {
                 imageFile
             )
 
-            mCurrentPhotoPath = imageFile.absolutePath
-
-            takePicture.launch(photoURI)
+            val mCurrentPhotoPath = imageFile.absolutePath
+            activityCallBack!!.onNewObservationButtonClick(mCurrentPhotoPath, photoURI)
         }
     }
 
-    private val takePicture =
-        registerForActivityResult(ActivityResultContracts.TakePicture()) { result ->
-            if (result) {
-                activityCallBack!!.onNewObservationButtonClick(mCurrentPhotoPath)
-            }
+    override fun onLocationChanged(p0: Location) {
+        if (isAdded) {
+            viewModel.getWeatherLatLon(p0.latitude, p0.longitude)
+            viewModel.hits.observe(requireActivity(), {
+                view?.findViewById<TextView>(R.id.descriptionTextView)?.text =
+                    it.weather[0].description
+                view?.findViewById<TextView>(R.id.temperatureTextView)?.text =
+                    getString(R.string.temperature_text, String.format("%.0f", it.main.temp))
+                view?.findViewById<TextView>(R.id.pressureTextView)?.text =
+                    getString(R.string.pressure_text, it.main.pressure.toString())
+                view?.findViewById<TextView>(R.id.humidityTextView)?.text =
+                    getString(R.string.humidity_text, it.main.humidity.toString())
+                view?.findViewById<TextView>(R.id.windSpeedTextView)?.text =
+                    getString(R.string.wind_speed_text, it.wind.speed.toString())
+                view?.findViewById<TextView>(R.id.windDirectionTextView)?.text =
+                    getString(R.string.wind_direction_text, it.wind.deg.toString())
+                view?.findViewById<TextView>(R.id.placeNameCountryTextView)?.text =
+                    getString(R.string.place_name_country_text, it.name, it.sys.country)
+
+                lifecycleScope.launch(Dispatchers.Main) {
+                    val iconImageBitmap = withContext(Dispatchers.IO) {
+                        WeatherIconApi.getWeatherIcon(it.weather[0].icon)
+                    }
+                    if (iconImageBitmap != null) {
+                        view?.findViewById<ImageView>(R.id.iconImageView)
+                            ?.setImageBitmap(iconImageBitmap)
+                    }
+                }
+            })
         }
 
-    override fun onLocationChanged(p0: Location) {
-        viewModel.getWeatherLatLon(p0.latitude, p0.longitude)
-        viewModel.hits.observe(requireActivity(), {
-            view?.findViewById<TextView>(R.id.descriptionTextView)?.text = it.weather[0].description
-            view?.findViewById<TextView>(R.id.temperatureTextView)?.text =
-                getString(R.string.temperature_text, String.format("%.0f", it.main.temp))
-            view?.findViewById<TextView>(R.id.pressureTextView)?.text =
-                getString(R.string.pressure_text, it.main.pressure.toString())
-            view?.findViewById<TextView>(R.id.humidityTextView)?.text =
-                getString(R.string.humidity_text, it.main.humidity.toString())
-            view?.findViewById<TextView>(R.id.windSpeedTextView)?.text =
-                getString(R.string.wind_speed_text, it.wind.speed.toString())
-            view?.findViewById<TextView>(R.id.windDirectionTextView)?.text =
-                getString(R.string.wind_direction_text, it.wind.deg.toString())
-            view?.findViewById<TextView>(R.id.placeNameCountryTextView)?.text =
-                getString(R.string.place_name_country_text, it.name, it.sys.country)
-
-            lifecycleScope.launch(Dispatchers.Main) {
-                val iconImageBitmap = withContext(Dispatchers.IO) {
-                    WeatherIconApi.getWeatherIcon(it.weather[0].icon)
-                }
-                if (iconImageBitmap != null) {
-                    view?.findViewById<ImageView>(R.id.iconImageView)
-                        ?.setImageBitmap(iconImageBitmap)
-                }
-            }
-        })
     }
 
     override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+    }
+
+    private fun checkLocationPermission() {
+        if ((Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED)
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                0
+            )
+        }
     }
 }
