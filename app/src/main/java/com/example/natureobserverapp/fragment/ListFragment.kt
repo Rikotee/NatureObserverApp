7818da0e3145b81521dae0e4323ba6e9050ef97b
Rikotee <teerek81@gmail.com>
2021-10-09 14:58:09 +0300

    time filter to listfragment

--- app/src/main/java/com/example/natureobserverapp/fragment/ListFragment.kt
+++ app/src/main/java/com/example/natureobserverapp/fragment/ListFragment.kt
@@ -16,17 +16,19 @@ import androidx.fragment.app.replace
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.natureobserverapp.Categories
-import com.example.natureobserverapp.R
-import com.example.natureobserverapp.RecyclerViewAdapter
+import com.example.natureobserverapp.*
 import com.example.natureobserverapp.model.NatureObservationsWithWeatherInfoModel
+import java.text.SimpleDateFormat
 import java.util.*
 
 class ListFragment : Fragment(), RecyclerViewAdapter.ClickListener {
 
     private lateinit var listSpinner: Spinner
+    private lateinit var timeFrameFilterSpinner: Spinner
     private val categoriesList: MutableList<String> = Categories.categories.toMutableList()
+    private val observationList = mutableListOf<NatureObservationWithWeatherInfo>()
     private var spinnerIndex: Int = 0
+    private var timeSpinnerIndex: Int = 0
     private val sharedPrefFile = "sharedpreference"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +46,10 @@ class ListFragment : Fragment(), RecyclerViewAdapter.ClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        timeFrameFilterSpinner = view.findViewById(R.id.timeFrameFilterSpinner)
         listSpinner = view.findViewById(R.id.listSpinner)
+
+        val timeFrames = listOf("All time", "This year", "This month", "This week", "Today")
 
         val aa = ArrayAdapter(
             requireContext(),
@@ -53,6 +58,14 @@ class ListFragment : Fragment(), RecyclerViewAdapter.ClickListener {
         )
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         listSpinner.adapter = aa
+
+        val aaT = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_item,
+            timeFrames
+        )
+        aaT.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        timeFrameFilterSpinner.adapter = aaT
 
         setSpinnerValue()
 
@@ -74,6 +87,27 @@ class ListFragment : Fragment(), RecyclerViewAdapter.ClickListener {
                 updateSpinner()
             }
         }
+
+        setTimeSpinnerValue()
+
+        timeFrameFilterSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    timeSpinnerIndex = position
+                    getList()
+
+                    updateTimeSpinner(position)
+                    //timeSpinnerIndex = position
+                }
+            }
     }
 
     private fun getList() {
@@ -89,11 +123,18 @@ class ListFragment : Fragment(), RecyclerViewAdapter.ClickListener {
 
             if (categoryS == "All") {
                 if (observationsRecyclerView != null) {
-                    observationsRecyclerView.adapter = RecyclerViewAdapter(it.reversed(), this)
+
+                    filterObservationsByTimeFrame(timeSpinnerIndex, it)
+                    observationsRecyclerView.adapter =
+                        RecyclerViewAdapter(observationList.reversed(), this)
+                    observationList.clear()
                 }
             } else {
                 if (observationsRecyclerView != null) {
-                    observationsRecyclerView.adapter = RecyclerViewAdapter(filtered.reversed(), this)
+                    filterObservationsByTimeFrame(timeSpinnerIndex, filtered)
+                    observationsRecyclerView.adapter =
+                        RecyclerViewAdapter(observationList.reversed(), this)
+                    observationList.clear()
                 }
             }
         }
@@ -127,11 +168,12 @@ class ListFragment : Fragment(), RecyclerViewAdapter.ClickListener {
         }
 
         if (oldCategories != null) {
-            for (item in oldCategories){
-                if (item !in categoriesList) { categoriesList.add(item) }
+            for (item in oldCategories) {
+                if (item !in categoriesList) {
+                    categoriesList.add(item)
+                }
             }
         }
-
         return categoriesList
     }
 
@@ -158,5 +200,78 @@ class ListFragment : Fragment(), RecyclerViewAdapter.ClickListener {
         val editor = sharedPreference?.edit()
         editor?.putInt("listSpinnerIndex", spinnerIndex)
         editor?.apply()
+    }
+
+    private fun setTimeSpinnerValue() {
+        val sharedPreference =
+            this.activity?.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        val newSpinnerValue = sharedPreference?.getInt("TimeFrameFilterSpinnerIndex", 0)
+        if (newSpinnerValue != null) {
+            timeSpinnerIndex = newSpinnerValue
+            timeFrameFilterSpinner.setSelection(newSpinnerValue)
+        }
+    }
+
+    private fun updateTimeSpinner(spinnerIndex: Int) {
+        val sharedPreference =
+            this.activity?.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        val editor = sharedPreference?.edit()
+        editor?.putInt("TimeFrameFilterSpinnerIndex", spinnerIndex)
+        editor?.apply()
+    }
+
+    private fun filterObservationsByTimeFrame(
+        spinnerIndex: Int,
+        list: List<NatureObservationWithWeatherInfo>
+    ): MutableList<NatureObservationWithWeatherInfo> {
+        val selectedTimeFrame = timeFrameFilterSpinner.getItemAtPosition(spinnerIndex)
+
+        val currentDateCalendar = Calendar.getInstance()
+        val currentYear = currentDateCalendar.get(Calendar.YEAR)
+        val currentMonth = currentDateCalendar.get(Calendar.MONTH)
+        val currentWeek = currentDateCalendar.get(Calendar.WEEK_OF_YEAR)
+        val currentDay = currentDateCalendar.get(Calendar.DAY_OF_YEAR)
+
+        val formatter = SimpleDateFormat("d.M.yyyy hh.mm", Locale.getDefault())
+        val observationDateCalendar = Calendar.getInstance()
+
+        for (observation in list) {
+            val observationDate = formatter.parse(observation.natureObservation!!.dateAndTime)
+
+            if (observationDate != null) {
+                observationDateCalendar.time = observationDate
+                val observationYear = observationDateCalendar.get(Calendar.YEAR)
+                val observationMonth = observationDateCalendar.get(Calendar.MONTH)
+                val observationWeek = observationDateCalendar.get(Calendar.WEEK_OF_YEAR)
+                val observationDay = observationDateCalendar.get(Calendar.DAY_OF_YEAR)
+
+                when (selectedTimeFrame) {
+                    "This year" -> {
+                        if (observationYear == currentYear) {
+                            observationList.add(observation)
+                        }
+                    }
+                    "This month" -> {
+                        if (observationYear == currentYear && observationMonth == currentMonth) {
+                            observationList.add(observation)
+                        }
+                    }
+                    "This week" -> {
+                        if (observationYear == currentYear && observationWeek == currentWeek) {
+                            observationList.add(observation)
+                        }
+                    }
+                    "Today" -> {
+                        if (observationYear == currentYear && observationDay == currentDay) {
+                            observationList.add(observation)
+                        }
+                    }
+                    else -> {
+                        observationList.add(observation)
+                    }
+                }
+            }
+        }
+        return observationList
     }
 }
