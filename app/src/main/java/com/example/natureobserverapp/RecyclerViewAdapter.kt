2632bd0e6ce6706de7c00d08d6a2feaab1b50cb1
Rikotee <teerek81@gmail.com>
2021-10-12 16:06:50 +0300

    some accessibility features

--- app/src/main/java/com/example/natureobserverapp/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/natureobserverapp/RecyclerViewAdapter.kt
@@ -2,6 +2,7 @@ package com.example.natureobserverapp
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,25 +24,49 @@ class RecyclerViewAdapter(
 
     override fun getItemCount() = items?.size ?: 0
 
+    override fun onBindViewHolder(holder: ObservationViewHolder, position: Int) {
+        holder.bind()
+    }
+
     inner class ObservationViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var titleTextView: TextView = view.findViewById(R.id.titleTextView)
         var dayTextView: TextView = view.findViewById(R.id.dayTextView)
         var oImageView: ImageView = view.findViewById(R.id.oImageView)
-    }
 
-    override fun onBindViewHolder(holder: ObservationViewHolder, position: Int) {
-        holder.titleTextView.text = items?.get(position)?.natureObservation?.title
-        holder.dayTextView.text = items?.get(position)?.natureObservation?.dateAndTime
-        holder.oImageView.setImageBitmap(image(position))
+        fun bind() {
+            titleTextView.text = items?.get(position)?.natureObservation?.title
+            dayTextView.text = items?.get(position)?.natureObservation?.dateAndTime
+            oImageView.setImageBitmap(image(position))
 
-        holder.itemView.setOnClickListener {
-            clickListener.onItemClick(items?.get(position)?.natureObservation?.id)
+            itemView.setOnClickListener {
+                clickListener.onItemClick(items?.get(position)?.natureObservation?.id)
+            }
         }
     }
-
+    
     private fun image(position: Int): Bitmap? {
         val pictureFilePath = items?.get(position)?.natureObservation?.picturePath
-        return BitmapFactory.decodeFile(pictureFilePath)
+        val imageBitmap = BitmapFactory.decodeFile(pictureFilePath)
+        return getResizedBitmap(imageBitmap, 200)
+    }
+
+    private fun getResizedBitmap(image: Bitmap, maxSize: Int): Bitmap? {
+        var width = image.width
+        var height = image.height
+        val bitmapRatio = width.toFloat() / height.toFloat()
+        if (bitmapRatio > 1) {
+            width = maxSize
+            height = (width / bitmapRatio).toInt()
+        } else {
+            height = maxSize
+            width = (height * bitmapRatio).toInt()
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true)
+    }
+
+    private fun Bitmap.rotate(degrees: Float): Bitmap {
+        val matrix = Matrix().apply { postRotate(degrees) }
+        return Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
     }
 
     interface ClickListener {
