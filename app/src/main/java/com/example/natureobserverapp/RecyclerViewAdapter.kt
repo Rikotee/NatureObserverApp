ec79a132afb23278faf7a5f3499ac4b22693fffe
Rikotee <teerek81@gmail.com>
2021-10-12 11:19:18 +0300

    some accessibility features

--- app/src/main/java/com/example/natureobserverapp/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/natureobserverapp/RecyclerViewAdapter.kt
@@ -1,20 +1,12 @@
 package com.example.natureobserverapp
 
-import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.drawable.Drawable
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.transition.Transition
 
 class RecyclerViewAdapter(
     private val items: List<NatureObservationWithWeatherInfo>?,
